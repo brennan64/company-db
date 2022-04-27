35840c7cf31fe51464e646d6928da003fa737f62
Brennan <brennanheley@gmail.com>
2022-04-27 01:10:17 -0500

    started from scratch

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,13 @@
-DROP DATABASE IF EXISTS emp_tracker_db;
-CREATE DATABASE emp_tracker_db;
+CREATE DATABASE employees_db;
 
-USE emp_tracker_db;
+USE employees_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE emp_role(
+CREATE TABLE job_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
@@ -16,13 +15,11 @@ CREATE TABLE emp_role(
     FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    FOREIGN KEY (role_id) REFERENCES emp_role(id)
+    FOREIGN KEY (role_id) REFERENCES job_role(id),
+    manager_id INT REFERENCES employee(id)
 )
-
--- index.js
+++ index.js
@@ -1,116 +1,17 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 require("dotenv").config();
-const Inquirer = require("inquirer");
+const prompts = require("./prompts");
 
-const db = require("./db");
-// console.log(db.viewAllDepts().then(data=>data));
-const menuQuestion = [
-  {
-    type: "list",
-    name: "menu",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Quit",
-    ],
-  },
-];
+const connection = mysql.createConnection({
+  host: "localhost",
+  // MySQL username,
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+});
 
-function init() {
-  Inquirer.prompt(menuQuestion).then((answers) => {
-    console.log(answers);
-    switchStatement(answers);
-  });
-}
-
-init();
-
-// function allDepts() {
-//   db.viewAllDepts().then((data) => console.log(data[0]));
-//   // menu()
-// }
-
-// console.log(allEmployee());
-
-function switchStatement(answers) {
-  switch (answers.menu) {
-    case "View All Employees":
-      allDepts();
-      break;
-    case "Add Employee":
-      addEmp();
-      break;
-    case "Update Employee Role":
-      updateEmp();
-      break;
-    case "View All Roles":
-      allRoles();
-      break;
-    case "Add Role":
-      addRoles();
-      break;
-    case "View All Departments":
-      allDepts();
-      break;
-    case "Add Department":
-      addDept();
-      break;
-    case "Quit":
-      quit();
-      break;
-  }
-}
-// allDepts: function () {
-//   db.query("SELECT * FROM department", function (err, results) {
-//     console.log(results);
-//   });
-// },
-
-// allRoles: function () {
-//   db.query(`SELECT * FROM emp_role`, function (err, results) {
-//     console.log(results);
-//   });
-// },
-
-// allEmps: function () {
-//   db.query("SELECT * FROM emp_role", function (err, results) {
-//     console.log(results);
-//   });
-// },
-
-function allEmps() {
-  console.log("--- Browsing All Employees ---");
-  db.findAllEmployees()
-    .then(([employeeData]) => {
-      let employees = employeeData;
-      console.table(employees);
-    })
-    .then(() => menu());
-}
-//   }
-
-//   addDept: function () {
-//     inquirer.prompt(prompts.newDep).then((ans) => console.log(ans));
-//   },
-
-//   addRole: function () {
-//     console.log("add role");
-//   },
-
-//   addEmp: function () {
-//     console.log("add employee");
-//   },
-
-//   updateRole: function () {
-//     console.log("new role");
-//   },
-
-//   quit: function () {
-//     console.log("good job");
-//   },
-// };
+connection.connect((err) => {
+  err ? console.log(err) : console.log(connected);
+});
+module.exports = connection;
